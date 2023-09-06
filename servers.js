7c6ffa893a07620d02e35f9c478ddbd35e0bad16
Homur Ve Gumur <99907332+HomurVeGomur@users.noreply.github.com>
2023-09-06 04:21:30 -0700

    Add files via upload

---
+++ servers.js
@@ -0,0 +1,17 @@
+// Copyright (c)2023 NeseOS Corporation. All rights reserved.
+
+const exp = require("express")
+
+
+var canRunXP = true;
+var createOS = nese.xp.ISO || en_nese_xp_professional_x64_dvd_final5.iso;
+var canVM = false;
+const run = run.en_nese_xp_professional_x64_dvd_final5.iso.x64;
+var country = USA;
+var changeLocation = false;
+var canTeleport = false;
+canRunXP.run.en_nese_xp_professional_x64_dvd_final5.iso;
+canRunXP.x64.iso;
+canRunXP.changeLocation.USA;
+canRunXP.system.en_nese_xp_professional_x64_dvd_final5;
+var canRunXP = run.en_nese_xp_professional_x64_dvd_final5.iso;
